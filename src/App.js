1c309e2c264cb084b6a86031379153dde82f92ee
Orie Steele <orie@transmute.industries>
2020-03-01 12:59:53 -0600

    chore: wip

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import GithubCorner from 'react-github-corner';
+import Button from '@material-ui/core/Button';
+
 import logo from './logo.svg';
 import './App.css';
 
 const cachedUrl = 'https://pwa.transmute.world/logo-cached.svg'
 
 function App() {
+
+  const [state, setState] = React.useState({});
+
   return (
     <div className="App">
       <GithubCorner bannerColor={'#64CEAA'} href="https://github.com/OR13/react-pwa" />
-      <header className="App-header">
+      <section className="sw-section">
+        <h3>Serving SVGs from SW...</h3>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          style={{marginBottom: '16px'}}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <img src={`${cachedUrl}`} className="App-logo" alt="service-worker-logo" />
-      </header>
+      </section>
+
+      <section className="sw-section">
+        <h3>HTTP Interceptor...</h3>
+        <Button variant="contained" color="primary" onClick={async ()=>{
+  
+          const data = await fetch('https://pwa.transmute.world/kms/keystore').then((res)=>{
+            return res.json()
+          });
+        
+          setState({
+            data
+          })
+        }}>
+          Make Request
+        </Button>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
+      </section>
     </div>
   );
 }
