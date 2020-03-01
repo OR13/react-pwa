986b0d528a69d8903609fbc6cc5330e87c9050cf
Orie Steele <orie@transmute.industries>
2020-03-01 12:59:35 -0600

    Updates

---
+++ precache-manifest.47773ea4491a357d4176cc1b23757336.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457e70840641e3f43d1faf6da824ae7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e2cc591e3a0c771723a",
+    "url": "/static/css/main.d4abe781.chunk.css"
+  },
+  {
+    "revision": "68bd3eeffad677ad57cd",
+    "url": "/static/js/2.46082fd5.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.46082fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2cc591e3a0c771723a",
+    "url": "/static/js/main.722a5dbe.chunk.js"
+  },
+  {
+    "revision": "e1405c2239592115999c",
+    "url": "/static/js/runtime-main.3630572f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
