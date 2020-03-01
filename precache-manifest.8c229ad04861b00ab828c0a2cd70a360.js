a7a3ebd56575d0a63c5ea656e847e5bc58619f03
Orie Steele <orie@transmute.industries>
2020-03-01 11:50:42 -0600

    Updates

---
+++ precache-manifest.8c229ad04861b00ab828c0a2cd70a360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0d59e65042b3b773c2f6ebe6f4aae6",
+    "url": "/react-pwa/index.html"
+  },
+  {
+    "revision": "4845a51d31fcd48b5f6a",
+    "url": "/react-pwa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4cd7d64f00c7405d35db",
+    "url": "/react-pwa/static/js/2.625aaeaf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-pwa/static/js/2.625aaeaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4845a51d31fcd48b5f6a",
+    "url": "/react-pwa/static/js/main.df778c91.chunk.js"
+  },
+  {
+    "revision": "72188a7c0703b00e8254",
+    "url": "/react-pwa/static/js/runtime-main.5d797ca6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
